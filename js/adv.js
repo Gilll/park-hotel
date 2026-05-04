afa9fbaafb16f9c0ff515e615a2efe96abde3f57
Gilll <4Life@tut.by>
2026-05-04 08:10:55 +0300

    update

--- js/adv.js
+++ js/adv.js
@@ -7,11 +7,33 @@ $(document).ready(() => {
         },
         slidesPerView: 5
     });
-    Fancybox.bind('[data-fancybox="gallery"]', {
-        Carousel: {
-            Thumbs: {
-                type: "classic"
+    const menuSlider1 = new Swiper('.menu-slider-1', {
+        navigation: {
+            nextEl: '.ms1-next',
+            prevEl: '.ms1-prev',
+        },
+        slidesPerView: 1,
+        loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 1.6
             }
         }
     });
+    const menuSlider2 = new Swiper('.menu-slider-2', {
+        navigation: {
+            nextEl: '.ms2-next',
+            prevEl: '.ms2-prev',
+        },
+        slidesPerView: 1,
+        loop: true,
+        breakpoints: {
+            768: {
+                slidesPerView: 1.7
+            }
+        }
+    });
+    $(".faq-list-item .row").click(function () {
+        $(this).toggleClass("active").next().slideToggle()
+    })
 })
